54cf2ee4e751872256ead9f569ba5fd92b000ab9
Nelson Ribeiro Reis Filho <kommander@localhost.localdomain>
2020-03-19 17:49:33 -0300

    Adicionado o suporte ao sensor de fim de curso

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # GarageHub
-  Sistema composto por sensor de distância e LEDs RGB para auxilio no estacionamento
-em garagens apertadas
+    Sistema conectado de estacionamento com indicador de distância de parada e luz
+auxiliar para serviço.
+
+    O GarageHub é composto por um controlador ESP32, um sensor de distância ultrassônico,
+fita de led RGB (que funciona como indicador de distância e luz auxiliar de serviço) e
+sensor fim de curso magnético.
+
+    Através destes componentes o GarageHub consegue comunicar se o portão está aberto ou fechado,
+indica quando você está na distância ideal da parede e ainda serve como iluminação auxiliar para
+trabalhar no seu veículo.
+
+    Você consegue saber se o portão de casa está aberto ou fechado e alterar a distância através da
+aplicação web que pode ser acessada através do endereço de ip impresso na porta serial durante o boot
 
 
 ## TO DO:
-* Ajustar os limiares das cores de modo que eles tomem como base a distância alvo
-* Alterar o código para funcionar com ESP32 (atualmente rodando no arduino uno)
+* ~~Ajustar os limiares das cores de modo que eles tomem como base a distância alvo~~
+* ~~Alterar o código para funcionar com ESP32 (atualmente rodando no arduino uno)~~
 * Implementar o WebServer e colocar a página html no ar
-* Adicionar suporte ao sensor de fim de curso do portão
-* Limitar as luzes do modo distância a funcionar apenas com o portão aberto
+* ~~Adicionar suporte ao sensor de fim de curso do portão~~
+* ~~Limitar as luzes do modo distância a funcionar apenas com o portão aberto~~
 * Adicionar modo iluminação auxiliar (luz branca)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <stdint.h>
 
 // PINOUT
+uint8_t sensPin = 4;
 uint8_t redPin = 16;
 uint8_t greenPin = 17;
 uint8_t bluePin = 5;
@@ -15,9 +17,12 @@ const uint8_t redChannel = 0;
 const uint8_t blueChannel = 1;
 const uint8_t greenChannel = 2;
 
-
+// Program vars
+bool auxLight = false;
 uint32_t printTimer = 0;
-double distancia;
+double distancia, distAlvo;
+// Pagina web a ser adicionada
+char* html = "";
 
 void fadeFromTo(int from, int to){
 	for(int i = 0; i < 255; i++){
@@ -48,14 +53,8 @@ double calcDist(){
 }
 
 void setup(){
-	// Teste da fita (ascender branca)
-	/*pinMode(greenPin, OUTPUT);
-	pinMode(bluePin, OUTPUT);
-	pinMode(redPin, OUTPUT);
-	digitalWrite(greenPin, 1);
-	digitalWrite(bluePin, 1);
-	digitalWrite(redPin, 1);*/
-
+	// Inicializando sensor
+	pinMode(sensPin, INPUT);
 	// Inicializando os PWM
 	ledcAttachPin(redPin, redChannel);
 	ledcAttachPin(bluePin, blueChannel);
@@ -68,38 +67,29 @@ void setup(){
 	pinMode(echo, INPUT);
 	// Garantindo que o gatilho está baixo antes do incio
 	digitalWrite(trigger, LOW);
+	// Setando distancia alvo para o padrao (5cm)
+	distAlvo = 5.0;
 	// Abrindo comunicação serial com baudrate 115200
 	Serial.begin(115200);
 	Serial.println("Setup feito com sucesso!");
 }
 
 void loop(){
-		
-		// Codigo de exemplo alteração de cores analogica
-		/*fadeFromTo(blueChannel, greenChannel);
-		Serial.println("I'm faded");
-		fadeFromTo(greenChannel, redChannel);
-		Serial.println("Where are you now?");
-		fadeFromTo(redChannel, blueChannel);
-		Serial.println("Under de seaa");*/
-		
-		/* Codigo de exemplo calculo de distancia
-		if(millis() - printTimer > 500){
-			printTimer = millis();
-			double distancia = 0;
-			distancia = calcDist();
-			Serial.print(distancia);
-			Serial.println(" cm");
-		}*/
+	if(!sensPin){	
 		distancia = calcDist();
-		if (distancia > 50){
+		if (distancia > distAlvo*3){
 			setRGB(0, 255, 0);
-		} else if (distancia <= 50 && distancia > 20){
+		} else if (distancia <= distAlvo*3 && distancia > distAlvo+1){
 			setRGB(129, 126, 0);
 		} else {
 			setRGB(255, 0, 0);
 		}
 		Serial.print(distancia);
 		Serial.println(" cm");
+	} else if(auxLight){
+		setRGB(255, 255, 255);
+	} else {
+		setRGB(0, 0, 0);
+	}
 }
 
