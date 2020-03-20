9f1cba8803381f73f01f3c60888e4b723ac731b5
Nelson Ribeiro Reis Filho <kommander@localhost.localdomain>
2020-03-19 23:24:25 -0300

    Feita a implementação inicial da interface web

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # GarageHub
-    Sistema conectado de estacionamento com indicador de distância de parada e luz
+Sistema conectado de estacionamento com indicador de distância de parada e luz
 auxiliar para serviço.
 
-    O GarageHub é composto por um controlador ESP32, um sensor de distância ultrassônico,
+O GarageHub é composto por um controlador ESP32, um sensor de distância ultrassônico,
 fita de led RGB (que funciona como indicador de distância e luz auxiliar de serviço) e
 sensor fim de curso magnético.
 
-    Através destes componentes o GarageHub consegue comunicar se o portão está aberto ou fechado,
+Através destes componentes o GarageHub consegue comunicar se o portão está aberto ou fechado,
 indica quando você está na distância ideal da parede e ainda serve como iluminação auxiliar para
 trabalhar no seu veículo.
 
-    Você consegue saber se o portão de casa está aberto ou fechado e alterar a distância através da
+Você consegue saber se o portão de casa está aberto ou fechado e alterar a distância através da
 aplicação web que pode ser acessada através do endereço de ip impresso na porta serial durante o boot
 
 
 ## TO DO:
 * ~~Ajustar os limiares das cores de modo que eles tomem como base a distância alvo~~
 * ~~Alterar o código para funcionar com ESP32 (atualmente rodando no arduino uno)~~
-* Implementar o WebServer e colocar a página html no ar
+* ~~Implementar o WebServer e colocar a página html no ar~~
 * ~~Adicionar suporte ao sensor de fim de curso do portão~~
 * ~~Limitar as luzes do modo distância a funcionar apenas com o portão aberto~~
-* Adicionar modo iluminação auxiliar (luz branca)
+* ~~Adicionar modo iluminação auxiliar (luz branca)~~
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <stdint.h>
+#include <string.h>
 
 // PINOUT
 uint8_t sensPin = 4;
@@ -21,8 +22,14 @@ const uint8_t greenChannel = 2;
 bool auxLight = false;
 uint32_t printTimer = 0;
 double distancia, distAlvo;
+
+// Configuração do wifi
+const char* ssid = "Nelson";
+const char* password = "Kommander030500";
+WiFiServer server(80);
+
 // Pagina web a ser adicionada
-char* html = "";
+const char* html = "<html><head> <title>GarageHub</title> <meta charset=\"UTF-8\" name=\"viewport\" content=\"width=device-width, initial-scale=1\"> <link rel=\"icon\" href=\"data:,\"> <style> html { font-family: Helvetica; display: inline-block; justify-content: center; justify-items: center; background-color: #2C2F33; margin: 0px auto; text-align: center; } h1 { color: white; background-color: #23272A; padding-top: 1.2vh; padding-bottom: 1.2vh; font-size: 1.5rem; border: none; position: absolute; top: 0; left: 0; width: 100%; } h2 { color: white; padding-top: 1vh; padding-bottom: 0.3vh; padding-left: 2vw; padding-right: 2vw; font-size: 1.5rem; } p { font-size: 1rem; color: white; } div { background-color: #23272A; margin-top: 30vh; padding-left: 9px; padding-right: 9px; padding-bottom: 1vh; border: none; border-radius: 30px; display: inline-block; justify-content: center; justify-self: center; } .indicador { color: #7286DA; font-size: 1.2rem; } .on { display: inline-block; background-color: #7286DA; border: none; border-radius: 10px; color: white; padding: 0.57vh 0.6vw; text-decoration: none; font-size: 0.8rem; margin: 2px; cursor: pointer; } .off { display: inline-block; background-color: white; border: none; border-radius: 8px; color: #2C2F22; padding: 0.57vh 0.6vw; text-decoration: none; font-size: 0.8rem; cursor: pointer; } .popup { margin-top: 5vh; visibility: hidden; } </style></head><body> <h1>GarageHub</h1> <div> <h2>Distância Alvo:</h2> <p> <l id=\"distAlvo\" class=\"indicador\">0.0cm</l> <button onclick=\"editPopupShow()\" class=\"off\">Editar</button> </p> </div> <div> <h2>Distância Atual:</h2> <p> <l id=\"distancia\" class=\"indicador\">0.0cm</l> </p> </div> <div> <h2>Estado do portão:</h2> <p> <l id=\"statPortao\" id=\"portao\" class=\"indicador\">Fechado</l> </p> </div> <p><button onclick=\"toggleAux()\" class=\"off\">Luz auxiliar</button></p> <p id=\"myPopup\" class=\"popup\">Novo Alvo: <input id=\"inputTarget\" type=\"text\"><br> <button onclick=\"submit()\" class=\"on\">Ok</button> <button onclick=\"editPopupHide()\" class=\"off\">Cancelar</button> </p> <script> function SubscribePane(url) { function showMessage(message) { let alvoElem = document.getElementById(\"distAlvo\"); let distanciaElem = document.getElementById(\"distancia\"); let portaoElem = document.getElementById(\"statPortao\"); distanciaElem.innerHTML = message[\"distancia\"] + \"cm\"; portaoElem.innerHTML = message[\"portao\"] == 0 ? \"Aberto\" : \"Fechado\"; alvoElem.innerHTML = message[\"alvo\"] + \"cm\"; } async function subscribe() { let response = await fetch(url); if (response.status == 502) { await subscribe(); } else if (response.status != 200) { showMessage(response.statusText); await new Promise(resolve => setTimeout(resolve, 1000)); await subscribe(); } else { let dirtyMessage = await response.text(); let message = JSON.parse(dirtyMessage); showMessage(message); await subscribe(); } } subscribe(); } function editPopupShow() { let popup = document.getElementById(\"myPopup\"); popup.style.visibility = \"visible\"; } function editPopupHide() { let popup = document.getElementById(\"myPopup\"); popup.style.visibility = \"hidden\"; } function submit() { let input = document.getElementById(\"inputTarget\"); let inputData = input.value; console.log(inputData); var http = new XMLHttpRequest();http.open(\"POST\", \"/newtarget\", true);http.setRequestHeader(\"Content-type\", \"text/plain\");http.send(inputData);http.onload = function () {let messageElem = document.getElementById(\"distAlvo\");let message = http.responseText;messageElem.innerHTML = message;}; let popup = document.getElementById(\"myPopup\"); popup.style.visibility = \"hidden\"; } function toggleAux(){ var http = new XMLHttpRequest(); http.open(\"GET\", \"/toggleAux\"); http.send(); } new SubscribePane('/?update'); </script></body></html>";
 
 void fadeFromTo(int from, int to){
 	for(int i = 0; i < 255; i++){
@@ -52,6 +59,30 @@ double calcDist(){
 	return distance;
 }
 
+void lerOCorpo(WiFiClient client){
+	String currentLine, lastLine;
+	while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+			if(currentLine.length() == 0){
+				distAlvo = lastLine.toDouble();
+			} else {
+				currentLine = "";
+			}
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+	  } else {
+		  distAlvo = currentLine.toDouble();
+		  break;
+	  }
+	  lastLine = currentLine;
+	} 
+}
+
 void setup(){
 	// Inicializando sensor
 	pinMode(sensPin, INPUT);
@@ -69,13 +100,77 @@ void setup(){
 	digitalWrite(trigger, LOW);
 	// Setando distancia alvo para o padrao (5cm)
 	distAlvo = 5.0;
+
 	// Abrindo comunicação serial com baudrate 115200
 	Serial.begin(115200);
 	Serial.println("Setup feito com sucesso!");
+
+	// Connect to Wi-Fi network with SSID and password
+	Serial.print("Connecting to ");
+	Serial.println(ssid);
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED) {
+	  delay(500);
+	  Serial.print(".");
+	}
+	// Print local IP address and start web server
+	Serial.println("");
+	Serial.println("WiFi connected.");
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+	server.begin();
 }
 
 void loop(){
-	if(!sensPin){	
+	WiFiClient client = server.available();
+	// Existe um cliente, hora de atualizar os dados
+	if (client) {                             // if you get a client,
+    Serial.println("Cliente conectado!");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          currentLine = "";
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Checando se existe algum request e o tratando
+		if (currentLine.endsWith("GET / HTTP/1.1")){
+			client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            client.print(html);
+            client.println();
+            break;
+		}
+		if (currentLine.endsWith("POST /newtarget")){
+			lerOCorpo(client);
+			break;
+		}
+		if (currentLine.endsWith("GET /toggleAux")){
+			auxLight = !auxLight;
+			break;
+		}
+		if (currentLine.endsWith("GET /?update")){
+			client.printf("{\"alvo\" : \"%0.2f\", \"distancia\" : \"%0.2f\", \"portao\" : \"%d\"}", distAlvo, distancia, digitalRead(sensPin));
+			break;
+		}
+      }
+    }
+    // Fechando a conexão
+    client.stop();
+    Serial.println("Cliente desconectado!");
+  }
+	// Executa sem necessariamente ter um cliente
+	if (auxLight){
+		setRGB(255, 255, 255);
+	} else if (!digitalRead(sensPin)){	
 		distancia = calcDist();
 		if (distancia > distAlvo*3){
 			setRGB(0, 255, 0);
@@ -84,10 +179,8 @@ void loop(){
 		} else {
 			setRGB(255, 0, 0);
 		}
-		Serial.print(distancia);
-		Serial.println(" cm");
-	} else if(auxLight){
-		setRGB(255, 255, 255);
+		//Serial.print(distancia);
+		//Serial.println(" cm");
 	} else {
 		setRGB(0, 0, 0);
 	}
